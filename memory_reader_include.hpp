26b3a08eed6df9bccbbd19b63747f2a4d9338d98
Drumkq <dmitrinikitin1990@mail.ru>
2022-04-21 22:15:59 +0700

    Initial commit

---
+++ memory_reader_include.hpp
@@ -0,0 +1,6 @@
+#ifndef _H_MEMORYREADER_INCLUDE_
+#define _H_MEMORYREADER_INCLUDE_
+
+#include "src/memory_reader.hpp"
+
+#endif
---
+++ src/memory_reader.hpp
@@ -0,0 +1,10 @@
+#ifndef _H_MEMORYREADER_MEMORY_
+#define _H_MEMORYREADER_MEMORY_
+
+class memory
+{
+public:
+
+};
+
+#endif
